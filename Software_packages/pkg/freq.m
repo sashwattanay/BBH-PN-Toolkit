d2e1cfc04718c01c9cb9c40c7a70d3af429b6392
Rickmoy <88556102+Rickmoy@users.noreply.github.com>
2022-06-04 15:03:23 +0530

    Fifth action loop checked

--- Software_packages/pkg/freq.m
+++ Software_packages/pkg/freq.m
@@ -52,8 +52,12 @@ f3=-a2/(3 a3)+2 Sqrt[-p/3]Cos[1/3 ArcCos[(3 q)/(2p) Sqrt[-3/p]]+(3*2 \[Pi] )/3];
 
 k=(f2-f1)/(f3-f1);
 
-B1=(1/2)((SeffL +Ln^2 (Q1+Q2))(Jn+Ln)+Ln(Q1  S1n^2+Q2  S2n^2+(Q1+Q2)(\[CapitalDelta]2 Q1 -\[CapitalDelta]1 Q2)));B2=(1/2)((SeffL +Ln^2 (Q1+Q2))(Jn-Ln)-Ln(Q1  S1n^2+Q2  S2n^2+(Q1+Q2)(\[CapitalDelta]2 Q1 -\[CapitalDelta]1 Q2)));D1=Ln(Ln+Jn)+(\[CapitalDelta]2 Q1 -\[CapitalDelta]1 Q2);D2=Ln(Ln-Jn)+(\[CapitalDelta]2 Q1 -\[CapitalDelta]1 Q2);
-\[Alpha]1sq=((-f1+f2) (Q1-Q2))/(D1+f1 (-Q1+Q2)); \[Alpha]2sq=((-f1+f2) (Q1-Q2))/(D2+f1 (-Q1+Q2));
+B1=(1/2)((SeffL +Ln^2 (Q1+Q2))(Jn+Ln)+Ln(Q1  S1n^2+Q2  S2n^2+(Q1+Q2)(\[CapitalDelta]2 Q1 -\[CapitalDelta]1 Q2)));
+B2=(1/2)((SeffL +Ln^2 (Q1+Q2))(Jn-Ln)-Ln(Q1  S1n^2+Q2  S2n^2+(Q1+Q2)(\[CapitalDelta]2 Q1 -\[CapitalDelta]1 Q2)));
+D1=Ln(Ln+Jn)+(\[CapitalDelta]2 Q1 -\[CapitalDelta]1 Q2);
+D2=Ln(Ln-Jn)+(\[CapitalDelta]2 Q1 -\[CapitalDelta]1 Q2);
+\[Alpha]1sq=((f2-f1) (Q1-Q2))/(D1-f1 (Q1-Q2));
+\[Alpha]2sq=((f2-f1) (Q1-Q2))/(D2-f1 (Q1-Q2));
 
 
 
@@ -62,15 +66,20 @@ B1s1=1/2 (-S1n Q1 (Ln^2 -Jn S1n + S1n^2 +\[CapitalDelta]2 Q1)+(Jn-S1n)^2 S1n Q2-
 B2s1=1/2 (S1n Q1 (Ln^2 +Jn S1n + S1n^2 +\[CapitalDelta]2 Q1)-(Jn+S1n)^2 S1n Q2-(Jn+2S1n)\[CapitalDelta]1 Q1 Q2 +(Jn+S1n)\[CapitalDelta]1 Q2^2);
 D1s1=(S1n-Jn)S1n - \[CapitalDelta]1 Q2;
 D2s1=(S1n+Jn)S1n - \[CapitalDelta]1 Q2;
-\[Alpha]1s1sq=((-Q1)(f2-f1) )/(D1s1+f1 Q1); \[Alpha]2s1sq=((-Q1)(f2-f1))/(D2s1+f1 Q1);
+\[Alpha]1s1sq=((-Q1)(f2-f1) )/(D1s1+f1 Q1); 
+\[Alpha]2s1sq=((-Q1)(f2-f1))/(D2s1+f1 Q1);
 
 
-(*subspin space for spin S2*)B1s2 =  1/2 (-Jn^2 S2n Q1+2 Jn S2n^2 Q1-S2n^3 Q1+Jn \[CapitalDelta]2 Q1^2-S2n \[CapitalDelta]2 Q1^2+Ln^2 S2n Q2-Jn S2n^2 Q2+S2n^3 Q2-Jn \[CapitalDelta]2 Q1 Q2+2 S2n \[CapitalDelta]2 Q1 Q2-S2n \[CapitalDelta]1 Q2^2) ;
+(*subspin space for spin S2*)
+B1s2 =  1/2 (-Jn^2 S2n Q1+2 Jn S2n^2 Q1-S2n^3 Q1+Jn \[CapitalDelta]2 Q1^2-S2n \[CapitalDelta]2 Q1^2+Ln^2 S2n Q2-Jn S2n^2 Q2+S2n^3 Q2-Jn \[CapitalDelta]2 Q1 Q2+2 S2n \[CapitalDelta]2 Q1 Q2-S2n \[CapitalDelta]1 Q2^2) ;
 B2s2=1/2 (Jn^2 S2n Q1+2 Jn S2n^2 Q1+S2n^3 Q1+Jn \[CapitalDelta]2 Q1^2+S2n  \[CapitalDelta]2 Q1^2-Ln^2 S2n Q2-Jn S2n^2 Q2-S2n^3 Q2-Jn \[CapitalDelta]2 Q1 Q2-2 S2n \[CapitalDelta]2 Q1 Q2+S2n \[CapitalDelta]1 Q2^2)  ;
 D1s2= (Jn S2n-S2n^2-\[CapitalDelta]2 Q1);
 D2s2= (-Jn S2n-S2n^2-\[CapitalDelta]2 Q1);
 \[Alpha]1s2sq = ((-Q2)(f2-f1))/(D1s2+f1 Q2)  ;
 \[Alpha]2s2sq = ((-Q2)(f2-f1))/(D2s2+f1 Q2)  ;
+
+
+
 
 
 (*\[CapitalDelta]\[Lambda]s are the flow amount along each commuting constant *)
@@ -85,7 +94,7 @@ D2s2= (-Jn S2n-S2n^2-\[CapitalDelta]2 Q1);
 
 
 \[CapitalDelta]\[Lambda]4=(-1/(2 S1n))(4/Sqrt[A (f3-f1)] (-((B1s1 EllipticPi[\[Alpha]1s1sq,k])/(D1s1+f1 Q1))+(B2s1 EllipticPi[\[Alpha]2s1sq,k])/(D2s1+f1 Q1))+S1n (Q2-Q1) \[CapitalDelta]\[Lambda]1 ) ;(*Eq116*)
-\[CapitalDelta]\[Lambda]5=(-1/(2 S2n))(4/Sqrt[A (f3-f1)] (-((B1s2 EllipticPi[\[Alpha]1s1sq,k])/(D1s2+f1 Q2))+(B2s2 EllipticPi[\[Alpha]2s2sq,k])/(D2s2+f1 Q2))+S2n (Q1-Q2) \[CapitalDelta]\[Lambda]1 ) ;
+\[CapitalDelta]\[Lambda]5=(-1/(2 S2n))(4/Sqrt[A (f3-f1)] (-((B1s2 EllipticPi[\[Alpha]1s2sq,k])/(D1s2+f1 Q2))+(B2s2 EllipticPi[\[Alpha]2s2sq,k])/(D2s2+f1 Q2))+S2n (Q1-Q2) \[CapitalDelta]\[Lambda]1 ) ;
 
 
 
